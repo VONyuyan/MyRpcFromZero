197479659504323c6d0aea76ec3d7d1ea564444f
VONyuyan <18888916468@163.com>
2024-03-06 11:32:51 +0800

    version1: 增加服务端服务接口和方法，封装请求响应数据，统一数据格式，通过代理反射调用服务端方法，增加底层通信，负责发送请求和接受响应

--- src/main/java/com/fred/rpc/RPCServer.java
+++ src/main/java/com/fred/rpc/RPCServer.java
@@ -1,11 +1,15 @@
 package com.fred.rpc;
 
+import com.fred.rpc.pojo.RPCRequest;
+import com.fred.rpc.pojo.RPCResponse;
 import com.fred.rpc.pojo.User;
 import com.fred.rpc.service.impl.UserServiceImpl;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -32,13 +36,16 @@ public class RPCServer {
                     try {
                         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
                         ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                        // 读取客户端传过来的id
-                        Integer id = ois.readInt();
-                        User userByUserId = userService.getUserByUserId(id);
-                        // 写入User对象给客户端
-                        oos.writeObject(userByUserId);
+                        // 读取客户端传过来的request
+                        RPCRequest request = (RPCRequest) ois.readObject();
+                        // 反射调用对应方法
+                        Method method = userService.getClass().getMethod(request.getMethodName(), request.getParamsTypes());
+                        Object invoke = method.invoke(userService, request.getParams());
+                        // 封装，写入response对象
+                        oos.writeObject(RPCResponse.success(invoke));
                         oos.flush();
-                    } catch (IOException e){
+                    }catch (IOException | ClassNotFoundException | NoSuchMethodException | IllegalAccessException |
+                            InvocationTargetException e){
                         e.printStackTrace();
                         System.out.println("从IO中读取数据错误");
                     }
--- src/main/java/com/fred/rpc/service/UserService.java
+++ src/main/java/com/fred/rpc/service/UserService.java
@@ -14,4 +14,6 @@ import com.fred.rpc.pojo.User;
 public interface UserService {
     // 客户端通过这个接口调用服务端的实现类
     User getUserByUserId(Integer id);
+    // 给这个服务增加一个功能
+    Integer insertUserId(User user);
 }
